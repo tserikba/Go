58b4cf54352b6d70458b61a575633550c264f440
tony <togzhan.gil@gmail.com>
2022-10-23 15:44:38 +0600

    square root finding

---
+++ train/sqroot.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func squareRoot(number float64) (float64, error) {
+	if number < 0 {
+		return 0, fmt.Errorf("can't get square root of negative number")
+	}
+	return math.Sqrt(number), nil
+}
+func main() {
+	root, err := squareRoot(81)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%0.3f", root)
+	}
+}
