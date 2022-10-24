d0ad7c231c3d099592ce4609c6add3f89cd0be13
tony <togzhan.gil@gmail.com>
2022-10-24 21:17:06 +0600

    the first part:simple website

---
+++ www/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type User struct {
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
+	Hobbies               []string
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money "+
+		"equal: %d", u.Name, u.Age, u.Money)
+}
+
+func (u *User) setNewName(newName string) {
+	u.Name = newName
+}
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	bob := User{"Bob", 26, 98, 4.2, 0.9, []string{"Football", "Dance"}}
+	//bob.setNewName("Alex")
+	//fmt.Fprintf(w, `<b>Main Text</b>`)
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
+}
+
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Contacts page")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts/", contacts_page)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	handleRequest()
+}
